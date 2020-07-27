11e6fd9567308f637962c27c17f3ef97a9500d37
C0LD <coldmailgg@gmail.com>
2020-07-27 18:24:23 +0200

    washed colors fix

--- .screenlayout/layout.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-1 --off --output HDMI-1 --off --output DVI-D-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-1 --mode 1280x1024 --pos 1920x56 --rotate normal
